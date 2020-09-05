a63a7c94a356791dbe3dfe426ec9c9b2d26da37e
say_my_name_ <uchitnitk@gmail.com>
2020-09-05 13:49:14 +0530

    overall structure made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 class App extends React.Component {
   render() {
     return(
-      <div>hello</div>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
     )  
   }
 }
